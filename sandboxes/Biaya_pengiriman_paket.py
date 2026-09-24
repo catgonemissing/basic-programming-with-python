ff386241bfda76e9d55b7152714e55ada7f4420c
catgonemissing <catgonemissinggg@gmail.com>
2026-09-24 19:58:48 +0800

    Code improvement

--- sandboxes/Biaya_pengiriman_paket.py
+++ sandboxes/Biaya_pengiriman_paket.py
@@ -1,12 +1,16 @@
-Berat_paket = int(input("Berapa berat paketmu dalam kg? "))
+try:
+    berat_paket = float(input("Berapa berat paketmu dalam kg? "))
+    
+    if berat_paket < 0:
+        print("Input tidak valid: Angka negatif tidak diperkenankan.")
+    elif berat_paket <= 2:
+        print("Biaya pengiriman adalah RP. 10,000")
+    elif berat_paket <= 5:
+        print("Biaya pengiriman adalah RP. 20,000")
+    elif berat_paket <= 10:
+        print("Biaya pengiriman adalah RP. 35,000")
+    else:
+        print("Berat paket melebihi batas layanan")
 
-if Berat_paket <= 0:
-    print("Negatif dan 0 tidak boleh!")
-elif Berat_paket > 0 and Berat_paket <= 2:
-    print("Biaya pengiriman adalah RP. 10,000")
-elif Berat_paket > 2 and Berat_paket <= 5:
-    print("Biaya pengiriman adalah RP. 20,000")
-elif Berat_paket > 5 and Berat_paket <= 10:
-    print("Biaya pengiriman adalah RP. 35,000")
-else:
-    print("Berat paket melebihi batas layanan")
+except ValueError:
+    print("Input tidak valid: Harap masukkan angka bulat.")
